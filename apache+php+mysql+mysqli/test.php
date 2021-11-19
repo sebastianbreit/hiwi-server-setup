1dc9c0a72b3ec83cb4285e9aef7ab288e8f8eaac
sebastianbreit <sebastianbreit311997@gmail.com>
2021-11-19 13:41:57 +0100

    update: re-added example setups, finalized proxy config

---
+++ apache+php+mysql+mysqli/test.php
@@ -0,0 +1,36 @@
+<?php
+//These are the defined authentication environment in the db service
+
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database user name
+$user = 'root';
+
+//database user password
+$pass = 'root';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection 1 failed: " . $conn->connect_error);
+} else {
+    echo "1: Connected to MySQL server successfully!";
+}
+
+$host = 'db';
+
+// Database use name
+$user = 'test';
+
+//database user password
+$pass = 'test';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection 2 failed: " . $conn->connect_error);
+} else {
+    echo "2: Connected to MySQL server successfully!";
+}
+?>
